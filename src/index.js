722ebfb752a2f1421013f86eab444964c45c19cc
Muhammed-Yousri <muhammed.yousri.sherif@gmail.com>
2025-03-24 00:04:17 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ResumeWebsite from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ResumeWebsite />
+  </React.StrictMode>
+);
